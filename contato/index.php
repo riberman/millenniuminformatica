1c7419ada6ee104c4c06bab3963e74acc4e4b700
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2020-03-12 20:22:32 -0300

    000 >> Html in Contato.

--- contato/index.php
+++ contato/index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contato</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Well done!</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+</div>
 	<?php 
 
 			//Carrega as classes do PHPMailer
@@ -22,3 +35,5 @@
 			//include("../root/envio.php");
 var_dump($_POST);
 	?>
+</body>
+</html>
