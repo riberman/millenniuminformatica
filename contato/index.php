303d673d7a1e43c5a55707a989faa97ae442ed82
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2020-03-12 20:07:57 -0300

    000 >> Change Form.

---
+++ contato/index.php
@@ -0,0 +1,24 @@
+	<?php 
+
+			//Carrega as classes do PHPMailer
+ 			//include("../root/phpmailer/class.phpmailer.php"); 
+ 			//include("../root/phpmailer/class.smtp.php"); 
+			
+			//envia o e-mail para o visitante do site
+			//$mailDestino = $_POST['txtEmail']; 
+			//$nome = $_POST['txtNome'];	
+			//$mensagem = "Obrigado pelo seu contato, responderemos em breve.";
+			//$assunto = "Obrigado pelo seu contato!";
+			//include("../root/envio.php");
+			
+			//envia o e-mail para o administrador do site
+			//$mailDestino = 'gorpaapps@gmail.com'; 
+			//$nome = 'GorpaApps';	
+			//$assunto = "Mensagem recebida do site";
+			//$mensagem = "Recebemos uma mensagem no site <br/>
+			//			<strong>Nome:</strong> $_POST[txtNome]<br/>
+			//			<strong>e-mail:</strong> $_POST[txtEmail]<br/>
+			//			<strong>mensagem:</strong> $_POST[txtMensagem]";
+			//include("../root/envio.php");
+var_dump($_POST);
+	?>
