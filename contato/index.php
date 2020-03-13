302db3cf5967a53b014756418c7e1338dd85c24e
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2020-03-12 21:38:26 -0300

    000 >> Correção Index.html form nome.

--- contato/index.php
+++ contato/index.php
@@ -10,6 +10,30 @@
 </script>
 </head>
 <body>
+
+<?php if (!empty($_POST['txtNome']) && !empty($_POST['txtEmail']) && !empty($_POST['txtMensagem'])): 
+			//Carrega as classes do PHPMailer
+ 			include("../../root/phpmailer/class.phpmailer.php"); 
+ 			include("../../root/phpmailer/class.smtp.php"); 
+			
+			//envia o e-mail para o visitante do site
+			$mailDestino = $_POST['txtEmail']; 
+			$nome = $_POST['txtNome'];	
+			$mensagem = "Obrigado pelo seu contato, responderemos em breve.";
+			$assunto = "Obrigado pelo seu contato!";
+			include("../../root/envio.php");
+			
+			//envia o e-mail para o administrador do site
+			$mailDestino = 'gorpaapps@gmail.com'; 
+			$nome = 'GorpaApps';	
+			$assunto = "Mensagem recebida do site";
+			$mensagem = "Recebemos uma mensagem no site <br/>
+						<strong>Nome:</strong> $_POST[txtNome]<br/>
+						<strong>e-mail:</strong> $_POST[txtEmail]<br/>
+						<strong>mensagem:</strong> $_POST[txtMensagem]";
+			include("../../root/envio.php");
+var_dump($_POST);
+?>
 <div class="alert alert-success" role="alert">
   <h4 class="alert-heading">Millennium Informática — Contato</h4>
   <p>O email de contato foi enviado com sucesso, por favor aguarde o recebimento do email de confirmação em sua caixa de entrada.</p>
@@ -18,29 +42,18 @@
   <hr>
   <button id="myButton" type="button" class="btn btn-primary">Voltar</button>
 </div>
-	<?php 
 
-			//Carrega as classes do PHPMailer
- 			//include("../root/phpmailer/class.phpmailer.php"); 
- 			//include("../root/phpmailer/class.smtp.php"); 
-			
-			//envia o e-mail para o visitante do site
-			//$mailDestino = $_POST['txtEmail']; 
-			//$nome = $_POST['txtNome'];	
-			//$mensagem = "Obrigado pelo seu contato, responderemos em breve.";
-			//$assunto = "Obrigado pelo seu contato!";
-			//include("../root/envio.php");
-			
-			//envia o e-mail para o administrador do site
-			//$mailDestino = 'gorpaapps@gmail.com'; 
-			//$nome = 'GorpaApps';	
-			//$assunto = "Mensagem recebida do site";
-			//$mensagem = "Recebemos uma mensagem no site <br/>
-			//			<strong>Nome:</strong> $_POST[txtNome]<br/>
-			//			<strong>e-mail:</strong> $_POST[txtEmail]<br/>
-			//			<strong>mensagem:</strong> $_POST[txtMensagem]";
-			//include("../root/envio.php");
-var_dump($_POST);
-	?>
+<?php else: ?>
+
+<div class="alert alert-danger" role="alert">
+  <h4 class="alert-heading">Millennium Informática — Contato</h4>
+  <p>Desculpe, mas não encontramos todos os dados necessários para enviar o email corretamente.</p>
+  <hr>
+  <p class="mb-0">Feche esta aba ou clique em voltar para ser redirecionado, e tente novamente.</p>
+  <hr>
+  <button id="myButton" type="button" class="btn btn-primary">Voltar</button>
+</div>
+
+<?php endif ?>
 </body>
 </html>
